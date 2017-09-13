1f119246cfd92d773644bddb628cfea42c605274
Rickard Sunden <rickard.sunden@outlook.com>
2017-09-13 12:46:02 -0400

    update navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import Button from './Button.js';
+
+import './css/Navbar.css';
 import logo from './img/logo.svg';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <div className="fluid-container">
-          <div className="grid">
-            <div className="column">
-              <img className="logo" src={logo} alt="Logo" />
-            </div>
-          </div>
+        <div className="left">
+          <img className="logo" src={logo} alt="Logo" />
+        </div>
+        <div className="right">
+          <Button text="download pdf version of site"/>
         </div>
       </div>
     );
