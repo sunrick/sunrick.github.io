655f13e5aaf529d7966d58e37181d061e0f32caf
Rickard Sunden <rickard.sunden@outlook.com>
2017-09-22 18:11:02 -0400

    start of animations

--- src/PortfolioItem.js
+++ src/PortfolioItem.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-
-import Button from './Button.js';
+import SVG from 'svg.js';
 
 import './css/PortfolioItem.css';
 
 
 class PortfolioItem extends Component {
+
+  componentDidMount() {
+    this.draw = SVG(this.svgEl).size(this.root.clientWidth, this.root.clientHeight);
+    let rect = this.draw.rect(this.root.clientWidth, this.root.clientHeight).attr({ fill: '#f06' });
+    for (let i = 0; i < 3; i++) {
+      let point = this.randomPoint();
+      let text = this.draw.text("forms express 2.0");
+      text.font({ family: "sans-serif", weight: "bold", opacity: 0.1, size: this.randomSize() })
+          .x(-30).y((i * .33)*this.root.clientHeight);
+      this.animateText(text);
+    }
+  }
+
+  animateText(text , delay) {
+    let randomPoint = this.randomPoint();
+    text.animate(10000).x(randomPoint.x).after(() => {
+      console.log(randomPoint);
+      this.animateText(text);
+    });
+  }
+
+  randomPoint() {
+    return {
+      x: Math.random() * this.root.clientWidth,
+      y: Math.random() * this.root.clientHeight
+    }
+  }
+
+  randomSize() {
+    return 48;
+  }
+
   render() {
     return (
-      <a href="/#" className="PortfolioItem">
+      <a style={{ position: "relative" }} href="/" className="PortfolioItem" ref={(el) => { this.root = el; }} >
         <div className="content">
           <h1> {this.props.title} </h1>
           <h3> {this.props.subtitle} </h3>
-          <div style={{marginTop: "5rem"}} className="buttons">
-            <Button to="/ruby-on-rails" text="ruby on rails"/>
-          </div>
+          <svg style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, zIndex: -1 }} ref={(el) => { this.svgEl = el; }}></svg>
         </div>
       </a>
     );
